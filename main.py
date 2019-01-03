fca5ee3814c5b4f17b74a1d048d92b5a3152873d
Fernando Espinosa <fespinosa@ucdavis.edu>
2019-01-03 12:54:56 -0800

    small

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ sudo pip install RPLCD
 pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
 pygame.mixer.init()
-GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 '''
 switch matrix format for each bank:
 (kick)       k1 k2 k3 k4 k5 k6
